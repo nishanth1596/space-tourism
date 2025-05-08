477348636d5c7dcfdd13f1f2f856cc0d3127a43f
Nishanth <nishanth1596@gmail.com>
2025-05-08 16:14:15 +0530

    refactor: render contents dynamically based on api data for nav button and images

--- src/pages/Destination/Destination.tsx
+++ src/pages/Destination/Destination.tsx
@@ -1,25 +1,32 @@
-import moonImg from "../../assets/destination/image-moon.webp";
-import marsImg from "../../assets/destination/image-mars.webp";
-import europaImg from "../../assets/destination/image-europa.png";
-import titanImg from "../../assets/destination/image-titan.png";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchData } from "../../api/fetchData";
+import LoadingSpinner from "../../ui/LoadingSpinner";
 
-const images = [
-  [moonImg, "Moon"],
-  [marsImg, "Mars"],
-  [europaImg, "Europa"],
-  [titanImg, "Titan"],
-];
+type DestinationProps = {
+  description: string;
+  distance: string;
+  images: {
+    png: string;
+    webp: string;
+  };
+  name: string;
+  travel: string;
+};
 
 function Destination() {
   const [imageIndex, setImageIndex] = useState<0 | 1 | 2 | 3>(0);
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["destination"],
+  const { data, isLoading } = useQuery({
+    queryKey: ["apiData"],
     queryFn: fetchData,
   });
+
+  if (isLoading) return <LoadingSpinner />;
+
+  const destinations = data.destinations;
+  const image = destinations[imageIndex].images.webp;
+  console.log(destinations);
 
   return (
     <section className="p-6 text-center">
@@ -29,19 +36,19 @@ function Destination() {
       </h1>
 
       <img
-        src={images[imageIndex][0]}
-        alt={`${images[imageIndex][1]} captured in the space`}
+        src={image}
+        alt={`${destinations[imageIndex].name} captured in the space`}
         className="mx-auto my-[50.5px] w-[150px]"
       />
 
       <nav className="flex justify-around pt-8">
-        {images.map((item, i) => (
+        {destinations.map((item: DestinationProps, i: number) => (
           <button
             onClick={() => setImageIndex(i as 0 | 1 | 2 | 3)}
             className="text-Blue-300 font-Barlow text-sm leading-4 font-normal tracking-[15%] uppercase"
             key={i}
           >
-            {item[1]}
+            {item.name}
           </button>
         ))}
       </nav>
