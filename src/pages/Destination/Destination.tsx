e8c87b9c3667f4bc9450b5a2cb2e07f585a298e9
Nishanth <nishanth1596@gmail.com>
2025-05-08 16:40:29 +0530

    feat: add layout for Destination component for mobile screen

--- src/pages/Destination/Destination.tsx
+++ src/pages/Destination/Destination.tsx
@@ -26,7 +26,6 @@ function Destination() {
 
   const destinations = data.destinations;
   const image = destinations[imageIndex].images.webp;
-  console.log(destinations);
 
   return (
     <section className="p-6 text-center">
@@ -52,6 +51,34 @@ function Destination() {
           </button>
         ))}
       </nav>
+
+      <h2 className="mt-6 text-[3.5rem] leading-16 font-normal uppercase">
+        {destinations[imageIndex].name}
+      </h2>
+
+      <p className="text-Blue-300 font-Barlow border-White/25 mt-4 border-b-[1px] pb-6 text-[0.94rem] leading-[1.8]">
+        {destinations[imageIndex].description}
+      </p>
+
+      <h3 className="font-Barlow text-Blue-300 mt-6 text-sm leading-4 font-normal tracking-[2px] uppercase">
+        Avg. distance
+        <span
+          className="text-White mt-3 block text-[1.75rem] leading-8 font-normal tracking-normal"
+          style={{ fontFamily: "var(--default-font-family)" }}
+        >
+          {destinations[imageIndex].distance}
+        </span>
+      </h3>
+
+      <h4 className="font-Barlow text-Blue-300 mt-6 text-sm leading-4 font-normal tracking-[2px] uppercase">
+        Est. travel time
+        <span
+          className="text-White mt-3 block text-[1.75rem] leading-8 font-normal tracking-normal"
+          style={{ fontFamily: "var(--default-font-family)" }}
+        >
+          {destinations[imageIndex].travel}
+        </span>
+      </h4>
     </section>
   );
 }
